f869b16e19d9bfa13ebf44812b3a660824d3641a
Tao Blancheton <tao.blancheton@epita.fr>
2019-03-05 23:31:55 +0100

    test

--- historique.php
+++ historique.php
@@ -51,7 +51,7 @@
                     // Connexion à la base de données
                     try
                     {
-                        $bdd = new PDO('mysql:host=localhost;dbname=BlindFlag;charset=utf8', 'root', 'password');
+                        $bdd = new PDO('mysql:host=localhost;dbname=BlindFlag;charset=utf8', 'blindflag', 'tao');
                     }
                     catch(Exception $e)
                     {
