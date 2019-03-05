f2ecacb12937fe4c3775a3e48013a58ce95b9a35
Tao Blancheton <tao.blancheton@epita.fr>
2019-03-05 23:12:13 +0100

    test

--- historique.php
+++ historique.php
@@ -43,46 +43,46 @@
 
 
             </header>
-            <article>
+                        <article>
                 <!-- Place du fil d'actualités, le plus récent en haut-->
                 <!-- Titre + photo si il y a + date + premières lignes du texte avec un bouton lire la suite sur qui redirige vers articles.html -->
 
                 <?php
-                	// Connexion à la base de données
-					try
-					{
-						$bdd = new PDO('mysql:host=localhost;dbname=BlindFlag;charset=utf8', 'root', 'password');
-					}
-					catch(Exception $e)
-					{
-						echo 'nope';
-        				die('Erreur : '.$e->getMessage());
-					}
+                    // Connexion à la base de données
+                    try
+                    {
+                        $bdd = new PDO('mysql:host=localhost;dbname=BlindFlag;charset=utf8', 'root', 'password');
+                    }
+                    catch(Exception $e)
+                    {
+                        echo 'nope';
+                        die('Erreur : '.$e->getMessage());
+                    }
 
-					// On récupère les 5 derniers article
-					$req = $bdd->query('SELECT DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date, title, article, picture_path FROM Article ORDER BY creation_date DESC LIMIT 0, 1');
+                    // On récupère les 5 derniers article
+                    $req = $bdd->query('SELECT DATE_FORMAT(C_date, \'%d/%m/%Y\') AS creation_date, title, article, picture_path FROM Article ORDER BY C_date DESC LIMIT 0, 5');
 
-					while ($donnees = $req->fetch())
-					{
-				?>
-						<div class="news">
-    						<p class="t3">
-        						<?php echo htmlspecialchars($donnees['title']); ?>
-        						<em>le <?php echo $donnees['creation_date']; ?></em>
-    						</p>
+                    while ($donnees = $req->fetch())
+                    {
+                ?>
+                        <div class="news">
+                            <p class="t3">
+                                <?php echo htmlspecialchars($donnees['title']); ?>
+                                <em>le <?php echo $donnees['creation_date']; ?></em>
+                            </p>
     
-    						<p>
-    							<?php
-    								echo nl2br(htmlspecialchars($donnees['text']));
-    							?>
-    							<br />
-    							<img src=<?php $donnees['picture_path'] ?>>
-    						</p>
-						</div>
-				<?php
-					}
-					$req->closeCursor();
-				?>
+                            <p>
+                                <?php
+                                    echo nl2br(htmlspecialchars($donnees['text']));
+                                ?>
+                                <br />
+                                <img src=<?php $donnees['picture_path'] ?>>
+                            </p>
+                        </div>
+                <?php
+                    }
+                    $req->closeCursor();
+                ?>
 
             </article>
  		</div>
