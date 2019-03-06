49f2f29ac6aa9dbd9535d9b315d8b5c2951df887
Tao Blancheton <tao.blancheton@epita.fr>
2019-03-06 17:19:21 +0100

    test

--- articles.php
+++ articles.php
@@ -22,6 +22,13 @@
             try
             {
                 $donnees = $bdd->query('SELECT id, DATE_FORMAT(C_date, \'%d/%m/%Y\') AS creation_date, Title, Text_article, Picture FROM Article WHERE id='. $_GET['ID']);
+
+            }
+            catch
+            {
+                echo "Erreur 404 - l'article n'existe pas...";
+                die('Erreur : '.$e->getMessage());
+            }
         ?>
 
                 <div class="article">
@@ -38,13 +45,6 @@
                         <img src=<?php echo $donnees['Picture'] ?>>
                     </p>
                 </div>
-        <?php
-            }
-            catch
-            {
-                echo "Erreur 404 - l'article n'existe pas...";
-            }
-        ?>
 
         <h1><!--Titre de l'article--></h1>
         <p><!-- Contenu entier de l'article--></p>
